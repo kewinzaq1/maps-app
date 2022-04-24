9252a20b3474f779aa4b1450ea60717e88384795
kewinzaq1 <kewinszlezingier@gmail.com>
2022-04-24 17:56:00 +0200

    UPDATE: map test

--- src/__tests__/Map.tsx
+++ src/__tests__/Map.tsx
@@ -1,25 +1,26 @@
-import { screen } from "@testing-library/react";
-import { render } from "../Utils";
+import {screen} from "@testing-library/react";
+import {render} from "../Utils";
 import Map from "../Components/Map";
 import "jest-canvas-mock";
 
 interface AutoComplete {
-  children: JSX.Element;
+    children: JSX.Element;
 }
 
 jest.mock("@react-google-maps/api", () => ({
-  useJsApiLoader: () => ({
-    isLoaded: true,
-    loadError: null,
-  }),
-  GoogleMap: () => <div></div>,
-  Autocomplete: ({ children }: AutoComplete) => <div>{children}</div>,
+    useJsApiLoader: () => ({
+        isLoaded: true,
+        loadError: null,
+    }),
+    GoogleMap: () => <div></div>,
+    Autocomplete: ({children}: AutoComplete) => <div>{children}</div>,
 }));
 
 test("render map", () => {
-  render(<Map />);
+    render(<Map/>);
 
-  expect(screen.getByText(/cost per km/i)).toBeInTheDocument();
-  expect(screen.getByText(/pln/i)).toBeInTheDocument();
-  expect(screen.getByText(/distance/i)).toBeInTheDocument();
+    expect(screen.getByText('Cost per km (PLN)', {selector: 'p'})).toBeInTheDocument()
+    expect(screen.getByText(/destination/i)).toBeInTheDocument();
+    expect(screen.getByText(/distance/i)).toBeInTheDocument();
+    expect(screen.getByRole('button', {name: /save results as pdf/i})).toBeInTheDocument()
 });
